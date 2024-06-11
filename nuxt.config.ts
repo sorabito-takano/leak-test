fa491414c13aa21748807439f4e2e516449d6305
sorabito-takano <h.takano@sorabito.com>
2024-06-11 22:50:42 +0900

    SPA mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   css: [
     "vuetify/lib/styles/main.sass"
   ],
